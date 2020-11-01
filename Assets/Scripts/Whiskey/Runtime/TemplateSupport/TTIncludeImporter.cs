6ff756a81a1109a055e89369f56b6ab0e5657b44
Ben Greenier <begreen@microsoft.com>
2020-11-01 13:10:10 -0800

    refactor

---
+++ Assets/Scripts/Whiskey/Runtime/TemplateSupport/TTIncludeImporter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.IO;
+
+#if UNITY_EDITOR
+using UnityEditor.Experimental.AssetImporters;
+
+/// <summary>
+/// A baby TTInclude importer so the editor can show ttinclude contents
+/// </summary>
+[ScriptedImporter(1, "ttinclude")]
+public class TTIncludeImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        var asset = new TextAsset(File.ReadAllText(ctx.assetPath));
+        ctx.AddObjectToAsset("text", asset);
+        ctx.SetMainObject(asset);
+    }
+}
+#endif
---
+++ Assets/Scripts/Whiskey/Tests/UnitTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using FasterGames.Whiskey;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        [Test]
+        public void Derivation()
+        {
+            var mock = new GameObject("mock");
+            var listener = mock.AddComponent<StringListener>();
+
+            Assert.IsInstanceOf<MonoBehaviour>(listener);
+            Assert.IsInstanceOf<ScriptableObject>(ScriptableObject.CreateInstance<StringEvent>());
+        }
+
+        [Test]
+        public void Trigger_Success()
+        {
+            var mock = new GameObject("mock");
+            var listener = mock.AddComponent<StringListener>();
+
+            var evt = ScriptableObject.CreateInstance<StringEvent>();
+            evt.Subscribe(listener);
+
+            string expectedData = "hello world";
+            string actualData = null;
+            listener.Reaction.AddListener((string data) =>
+            {
+                actualData = data;
+            });
+
+            evt.Trigger(expectedData);
+
+            Assert.AreEqual(expectedData, actualData);
+        }
+
+        [Test]
+        public void AutoSubscribe_Success()
+        {
+            var mock = new GameObject("mock");
+            var listener = mock.AddComponent<StringListener>();
+            var evt = ScriptableObject.CreateInstance<StringEvent>();
+            
+            listener.Event = evt;
+
+            // we reflect to get these, just like the UnityRuntime would for us.
+            // a caller should never need to do this, but we want to force unity runtime hooks for testing.
+            var OnEnable = typeof(StringListener).GetMethod("OnEnable", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            var OnDisable = typeof(StringListener).GetMethod("OnDisable", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+
+            string expectedData = "hello world";
+            List<string> actualData = new List<string>();
+            listener.Reaction.AddListener((string data) =>
+            {
+                actualData.Add(data);
+            });
+
+            OnEnable.Invoke(listener, new object[0]);
+
+            evt.Trigger(expectedData);
+
+            OnDisable.Invoke(listener, new object[0]);
+
+            evt.Trigger(expectedData);
+
+            Assert.AreEqual(1, actualData.Count);
+            Assert.AreEqual(expectedData, actualData[0]);
+        }
+    }
+}
